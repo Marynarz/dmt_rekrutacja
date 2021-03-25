2414b250962e3c9efbb9c8f755afb3f2b231b3f4
Wojtek <nwojtek@gmail.com>
2021-03-25 16:06:43 +0100

    do sprawdzenia

--- zad2.cpp/zad2.cpp
+++ zad2.cpp/zad2.cpp
@@ -2,8 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <ctime>
-#include <regex>
 
 const std::string range_file_path {"../data/rang.txt"};  //default rang file path
 const std::string test_file_path {"../data/track.txt"}; // default test file path
@@ -14,16 +12,18 @@ class Range
     std::string range_min;
     std::string range_max;
     std::string card_name;
+    std::ofstream & log_file
 
     public:
-    Range(std::string rng_min, std::string rng_max, std::string crd_name) : range_min {rng_min}, range_max {rng_max}, card_name {crd_name} {};
+    Range (const std::string & line, std::ofstream & lg_file);
+    Range(std::string rng_min, std::string rng_max, std::string crd_name, std::ofstream & lg_file) : range_min {rng_min}, range_max {rng_max}, card_name {crd_name} log_file {lg_file};
 
-    bool is_in_range();
+    bool is_in_range(const std::string & test_str);
     std::string get_card_name();
 }
 
 void load_card_numbers(std::vector<std::string> & buff, std::ofstream & log_file, const std::string path = test_file_path); // load file to vector
-void load_card_ranges(std::vector<ranges> & buff, stf::ofstream & log_file, const std::string path = range_file_path)       // load ranges
+void load_ranges(std::vector<Range> & buff, std::ofstream & log_file, const std::string path = range_file_path);
 
 int main()
 {
@@ -31,15 +31,26 @@ int main()
     std::ofstream log_file(log_file_path);
     log_file <<"Try to validate track2\n";
 
-    // vector with struct of card
+    // vector with cardnumbers and ranges
     std::vector<std::string> cards_nums;
+    std::vector<Range> ranges;
     
     // Magic
     load_card_numbers(cards_nums, log_file);
-    for(const card: cards_nums)
-        log_file <<"Loaded card: " <<card <<std::endl;
+    load_ranges(ranges, log_file);
 
-        
+    for(auto & crd: card_nums)
+    {
+        for(auto & rng: ranges)
+        {
+            if(rng.is_in_rnage(crd))
+            {
+                std::cout <<"Card name:\t" <<rng.get_card_name <<std::endl;
+                log_file <<"Card name:\t" <<rng.get_card_name <<std::endl;
+                break;
+            }
+        }
+    }
 
     std::cout <<"\nBye!\n";
     log_file <<"\nBye!\n";
@@ -51,10 +62,12 @@ void load_card_numbers(std::vector<std::string> & buff, std::ofstream & log_file
     log_file <<"\tLOAD_CARD_NUMBERS\nTrying to load file from path:\t" <<path <<std::endl;
     std::cout <<"\tLOAD_CARD_NUMBERS\nTrying to load file from path:\t" <<path <<std::endl;
     std::ifstream test_file(path);
+    
     if(test_file.is_open())
     {
         log_file <<"test file is opened\n";
         std::cout <<"test file is opened\n";
+        
         std::string line;
         while(std::getline(test_file, line))
         {
@@ -68,7 +81,66 @@ void load_card_numbers(std::vector<std::string> & buff, std::ofstream & log_file
         std::cout <<"Unable to open file!\n";
 }
 
-bool Range::is_in_range();
+void load_ranges(std::vector<Range> & buff, std::ofstream & log_file, const std::string path)
+{
+    log_file <<"\tLOAD_RANGES\nTrying to load file from path:\t" <<path <<std::endl;
+    std::cout <<"\tLOAD_RANGES\nTrying to load file from path:\t" <<path <<std::endl;
+    std::ifstream test_file(path);
+    
+    if(test_file.is_open())
+    {
+        log_file <<"test file is opened\n";
+        std::cout <<"test file is opened\n";
+        
+        // skipping first two lines
+        getline(test_file, line);
+        log_file <<"file version: " <<line <<std::endl;
+        std::cout <<"file version: " <<line <<std::endl;
+
+        getline(test_file, line);
+        log_file <<"file date: " <<line <<std::endl;
+        std::cout <<"file date: " <<line <<std::endl;
+
+        std::string line;
+        while(std::getline(test_file, line))
+        {
+            log_file <<"adding line to vector: " <<line <<std::endl;
+            std::cout <<"adding line to vector: " <<line <<std::endl;
+            buff.push_back(Range(line));
+        }        
+    }
+    else
+        std::cout <<"Unable to open file!\n";
+}
+
+Range::Range(const std::string & line, std::ofstream & lg_file)
+{
+    log_file = lg_file;
+    log_file <<"Creating object from line: " <<line <<std::endl;
+    size_t first_sep = line.find('|');
+    size_t sec_sep = line.find('|', first_sep + 1);
+    range_min = line.substr(1, first_sep - 1);
+    range_max = line.substr(first_sep + 1, sec_sep -1);
+    card_name = line.substr(sec_sep + 1);
+    log_file <<"range max:\t" <<range_max <<"\nrange min:\t" <<range_min <<"\ncard name:\t" <<card_name <<std::endl;
+}
+
+bool Range::is_in_range(const std::string & test_str)
+{
+    log_file <<"\n\tIS_IN_RANGE\nchecking: " <<test_str <<std::endl;
+    for(int letter = 0; letter < range_min.size(); letter++)
+    {
+        if(range_min[letter] >= test_str[letter] && range_max[letter] <= test_str[letter] )
+        {
+            log_file <<"Digit: '" <<test_str[letter] <<"' is NOT in range";
+            return false;
+        }
+        log_file <<"Digit: '" <<test_str[letter] <<"' is in range";
+    }
+    log_file <<"All line is in range\n";
+    return true;
+}
+
 std::string Range::get_card_name()
 {
     return card_name;
